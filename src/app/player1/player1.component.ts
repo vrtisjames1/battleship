c74fdb1c14b27c53af4aba36caf396dec988a3fb
James Vrtis <jamesvrtis@caskradmins-mbp.mynetworksettings.com>
2023-01-15 07:31:56 -0500

    updated information

--- src/app/player1/player1.component.ts
+++ src/app/player1/player1.component.ts
@@ -19,7 +19,7 @@ export class Player1Component {
   var2_array: any=["a1_2","a2_2","a3_2", "a4_2", "a5_2", "a6_2", "a7_2", "a8_2", "a9_2", "a10_2", "b1_2","b2_2","b3_2","b4_2", "b5_2", "b6_2", "b7_2", "b8_2", "b9_2", "b10_2", "c1_2","c2_2","c3_2","c4_2", "c5_2", "c6_2", "c7_2", "c8_2", "c9_2", "c10_2","d1_2","d2_2","d3_2","d4_2", "d5_2", "d6_2", "d7_2", "d8_2", "d9_2", "d10_2","e1_2","e2_2","e3_2","e4_2", "e5_2", "e6_2", "e7_2", "e8_2", "e9_2", "e10_2","f1_2","f2_2","f3_2","f4_2", "f5_2", "f6_2", "f7_2", "f8_2", "f9_2", "f10_2","g1_2","g2_2","g3_2","g4_2", "g5_2", "g6_2", "g7_2", "g8_2", "g9_2", "g10_2","h1_2","h2_2","h3_2","h4_2", "h5_2", "h6_2", "h7_2", "h8_2", "h9_2", "h10_2","i1_2","i2_2","i3_2","i4_2", "i5_2", "i6_2", "i7_2", "i8_2", "i9_2", "i10_2","j1_2","j2_2","j3_2","j4_2", "j5_2", "j6_2", "j7_2", "j8_2", "j9_2", "j10_2"]
   var_name: any = '';
   count: number = 0;
-  message: string = 'Click on the board to place the number of ships as shown in the photo. The ships can only be placed vertically or horizontally--not diagonally! Once complete, press the "submit" button below';
+  message: string = 'Click on the board to place the number of ships as shown in the photo. The ships may only be placed vertically or horizontally--not diagonally! Once complete, press the "submit" button below';
 
   submit(){
     // reset count
